a280ea2a246ce2c0a4284452fd72d6239398ebe7
= <=>
2023-10-21 14:35:46 -0300

    nuevo commit

--- src/app.js
+++ src/app.js
@@ -1,42 +1,51 @@
 import express from "express";
-import * as dotenv from "dotenv";
 import mongoose from "mongoose";
-import productsRouter from "./routes/products.routes.js";
-import cartsRouter from "./routes/carts.routes.js";
+import Loader from "./loader.js";
+import { userModel } from "./models/user.model.js";
+import { studentModel } from "./models/student.js";
+import { courseModel } from "./models/course.js";
+import * as dotenv from "dotenv";
 
 dotenv.config();
-const app = express();
-const PORT = process.env.SERVER_PORT || 8181;
+
+const PORT = process.env.PORT || 3131;
+//const MONGO_URL = process.env.MONGO_URL;
 const DB_USER = process.env.DB_USER;
 const DB_PASS = process.env.DB_PASS;
 const DB_NAME = process.env.DB_NAME;
+const app = express();
 
 app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
-
-app.use("/api/productos", productsRouter);
-app.use("/api/carrito", cartsRouter);
-
 const server = app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`);
 });
 
 server.on("error", (error) => console.log(`Error en servidor ${error}`));
-
 const environment = async () => {
-    try {
+try {
     await mongoose.connect(
         `mongodb+srv://${DB_USER}:${DB_PASS}@cluster0.pnpufdn.mongodb.net/${DB_NAME}?retryWrites=true&w=majority`
     );
     console.log("Conectado a MongoDB");
-    } catch (error) {
+
+
+    await courseModel.create({
+        title: "Curso de Backend",
+        description: "Curso de Backend con Node.js y MongoDB",
+        difficulty: 5,
+        topics: ["Node.js", "MongoDB", "Express"],
+        professor: "Mauricio",
+    });
+
+
+    let student = await studentModel.findOne({
+        _id: "63dc44b25e42d3e0d35039fc",
+    });
+    console.log(JSON.stringify(student, null, "\t"));
+
+} catch (error) {
     console.log(`Error al conectar a MongoDB: ${error}`);
-    }
+}
 };
 
-const isValidStartData = () => {
-    if (DB_PASS && DB_USER) return true;
-    else return false;
-};
-console.log("isValidStartData", isValidStartData());
-isValidStartData() && environment();
+environment();
